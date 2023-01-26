12a17d2bfe4bbf7a4a25b0063eab44853e98dcda
sami-theion <sami@theion.de>
2023-01-26 13:06:39 +0100

    added backend heating

--- includes/heating_controller.h
+++ includes/heating_controller.h
@@ -14,38 +14,55 @@
 #include <iostream>
 #include <string.h>
 #include "yaml-cpp/yaml.h"
-
+#include "subsystem_feedback.h"
 struct heating_config_yaml_params
 {
-
     double sulfur_temperature = 120;
-
-
+};
+struct heating_server
+{
+    const char* ip = "192.168.0.208";
+    uint16_t port = 8881;
 };
 class heating_controller
 {
 private:
- 
+    heating_server _heating_server;
+    sockpp::socket_initializer sockInit;
     bool heatingReady = false;
     YAML::Node config;
     heating_config_yaml_params _heating_params;
+    sockpp::tcp_connector* _heating_client;
+
+    std::map<u_int, std::string> heating_cmds = {
+         {0,"info"}, {1,"on"}, {2,"off"},
+        {3,"stay"}, {4,"temp?"},
+        {5,"temp"}, {6,"state?"}, 
+        {7,"plate_temp?"}, {8,"plate_temp"}
+    };
+    std::string heating_incoming_data;
+    u_int heating_data_length = 1024;
+    double sulfur_temperature = 0;
+
 public:
     heating_controller(/* args */);
     ~heating_controller();
     // controller
-    void heating_controller_connect();
+    wgm_feedbacks::enum_sub_sys_feedback heating_controller_connect();
+    void disconnect();
     void heating_controller_activate();
     void heating_controller_deactivate();
     void heating_controller_settemperature(double temp);
-
-
+    void heating_controller_setpaltetemperature(double temp);
     /********* helper functions */
     bool get_heating_controller_status();
     /*     helper getter */
     double get_heating_plate_temperature();
     double get_heating_sulfur_temperature();
     // direct call
-    void sendDirectCmd(std::string& cmd);
+    std::string sendDirectCmd(std::string cmd);
+    std::string waitForResponse();
+    void sendCmd(std::string& cmd, sockpp::tcp_connector* client, std::string args);
     void reload_config_file();
 };
 
--- src/heating_controller.cpp
+++ src/heating_controller.cpp
@@ -21,26 +21,95 @@ heating_controller::~heating_controller()
 {
 }
 
-void heating_controller::heating_controller_connect()
+void heating_controller::disconnect()
 {
+    if (!_heating_client) _heating_client->close();
+}
 
+wgm_feedbacks::enum_sub_sys_feedback heating_controller::heating_controller_connect()
+{
+    std::cout << "connecting to heating server" << std::endl;
+    _heating_client = new sockpp::tcp_connector({ _heating_server.ip, _heating_server.port });
+    // Implicitly creates an inet_address from {host,port}
+    // and then tries the connection.
+    if (!_heating_client) {
+        std::cerr << "Error connecting to server at "
+            << sockpp::inet_address(_heating_server.ip, _heating_server.port)
+            << "\n\t" << _heating_client->last_error_str() << std::endl;
+        return wgm_feedbacks::enum_sub_sys_feedback::sub_error;
+    }
+    std::cout << "Created a connection from " << _heating_client->address() << std::endl;
+    std::cout << "Created a connection to " << _heating_client->peer_address() << std::endl;
+    // Set a timeout for the responses
+    if (!_heating_client->read_timeout(std::chrono::seconds(5))) {
+        std::cerr << "Error setting timeout on TCP stream: "
+            << _heating_client->last_error_str() << std::endl;
+        return wgm_feedbacks::enum_sub_sys_feedback::sub_error;
+    }
+    heatingReady = true;
+    return wgm_feedbacks::enum_sub_sys_feedback::sub_success;
 }
 
 void heating_controller::heating_controller_activate()
 {
+    auto command = heating_cmds.find(1);
+    if (command != heating_cmds.end()) {
+        std::cout << "sending command: " << command->second << '\n';
+        sendDirectCmd(command->second);
+    }
 
 }
 
 void heating_controller::heating_controller_deactivate()
 {
-
+    auto command = heating_cmds.find(2);
+    if (command != heating_cmds.end()) {
+        std::cout << "sending command: " << command->second << '\n';
+        sendDirectCmd(command->second);
+    }
 }
 void heating_controller::heating_controller_settemperature(double temp)
 
 {
+    auto command = heating_cmds.find(5);
+    if (command != heating_cmds.end()) {
+        std::cout << "sending command: " << command->second << " args: " << temp << '\n';
+        std::string args = "=" + std::to_string(temp);
+        sendCmd(command->second, _heating_client, args);
+    }
+    _heating_params.sulfur_temperature = temp;
+}
+void heating_controller::heating_controller_setpaltetemperature(double temp)
+
+{
+    auto command = heating_cmds.find(8);
+    if (command != heating_cmds.end()) {
+        std::cout << "sending command: " << command->second << " args: " << temp << '\n';
+        std::string args = "=" + std::to_string(temp);
+        sendCmd(command->second, _heating_client, args);
+    }
     _heating_params.sulfur_temperature = temp;
 }
 
+double heating_controller::get_heating_plate_temperature()
+{
+   double duration = 0;
+    std::cout << "get axis curent position" << std::endl;
+    auto command = heating_cmds.find(7);
+    std::cout << "sending command: " << command->second << '\n';
+    auto resp =sendDirectCmd(command->second);
+    duration = std::stod(resp); // to double
+    return duration;
+}
+double heating_controller::get_heating_sulfur_temperature()
+{
+    std::cout << "get axis curent position" << std::endl;
+    auto command = heating_cmds.find(4);
+    std::cout << "sending command: " << command->second << '\n';
+    auto resp =sendDirectCmd(command->second);
+    sulfur_temperature = std::stod(resp); // to double
+    return sulfur_temperature;
+}
 /********* helper functions */
 
 bool heating_controller::get_heating_controller_status()
@@ -51,11 +120,48 @@ bool heating_controller::get_heating_controller_status()
 
 
 // direct call
-void heating_controller::sendDirectCmd(std::string& cmd)
+std::string heating_controller::sendDirectCmd(std::string cmd)
 {
-
+    if (_heating_client == nullptr) return "not connected";
+    std::cout << "sending heaitng command " << cmd << std::endl;
+    cmd = cmd + "\r\n";
+    if (_heating_client->write(cmd) != ssize_t(std::string(cmd).length())) {
+        std::cout << "Error writing to the TCP stream: "
+            << _heating_client->last_error_str() << std::endl;
+    }
+    return waitForResponse();
 }
+std::string heating_controller::waitForResponse()
+{
+    static int attempts = 0;
+    if (attempts == 10)
+    {
+        std::cout << "attempts: " << attempts << std::endl;
+        attempts = 0;
+        return "NA";
+    }
+    std::cout << "awaiting server response" << std::endl;
+    if (_heating_client->is_connected())
+    {
+        char Strholder[1024];
+        ssize_t n = _heating_client->read_n(&Strholder, 1024);
+        if (n > 0)
+        {
+            std::cout << "n bytes received: " << n << std::endl;
+            heating_incoming_data = Strholder;
+            heating_incoming_data.resize(n);
+            std::cout << "server replied : " << heating_incoming_data << std::endl;
+            return heating_incoming_data;
+        }
+        else
+        {
+            std::cout << "no server response " << n << std::endl;
+            attempts++;
+            return waitForResponse();
+        }
 
+    }
+}
 void heating_controller::reload_config_file()
 {
 
@@ -70,3 +176,13 @@ void heating_controller::reload_config_file()
 
 
 }
+
+void heating_controller::sendCmd(std::string& cmd, sockpp::tcp_connector* client, std::string args)
+{
+    if (client->write(cmd + args) != ssize_t(std::string(cmd + args).length())) {
+        std::cerr << "Error writing to the TCP stream: "
+            << client->last_error_str() << std::endl;
+    }
+    std::cout << "command " << cmd + args << " sent" << std::endl;
+}
+
